a499a8a4ebc30634a7052afbc276cef4b3a9f641
Wandia E Mugo <wwandia173@gmail.com>
2022-06-15 23:41:05 +0300

    commit tree 8872409f6734dd77d56109f06783402a958b2a70

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID');
+    
+      });
 }
 
 document.addEventListener('DOMContentLoaded', init);
